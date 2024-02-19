25373bd5f7942a3dffdc84ae09f358ba5d17a496
slappy163 <105318087+slappy163@users.noreply.github.com>
2024-02-19 14:10:33 -0700

    Started working on the UI for the events

---
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/EventListView.java
@@ -0,0 +1,14 @@
+package com.example.qrcheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list_view);
+    }
+}
---
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/createNewEventScreen2.java
@@ -0,0 +1,14 @@
+ package com.example.qrcheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNewEventScreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_event_screen2);
+    }
+}
