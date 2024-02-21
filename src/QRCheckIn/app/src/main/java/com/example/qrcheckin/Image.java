4e2278d1e48aa7fed483d03c7542c78dd9414dbb
shubhkaran30456 <shubhka1@ualberta.ca>
2024-02-21 01:47:19 -0700

    Created image class

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Image.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Image.java
@@ -1,2 +1,23 @@
-package com.example.qrcheckin;public class Image {
-}
+//package com.example.qrcheckin;
+//
+//import java.io.File;
+//
+//public class Image {
+//    private File imageFile;
+//    private Attendee uploader;
+//
+//    public Image(File imageFile, Attendee uploader) {
+//        this.imageFile = imageFile;
+//        this.uploader = uploader;
+//    }
+//
+//    public File getImageFile() {
+//        return imageFile;
+//    }
+//
+//    public Attendee getUploader() {
+//        return uploader;
+//    }
+//    public void deleteImage() {
+//    }
+//}
