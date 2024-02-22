7ccdc69587e1784d4d9700b32bcd7141cd941603
shubhkaran30456 <shubhka1@ualberta.ca>
2024-02-22 00:32:25 -0700

    Working on profilepicture

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/EventPoster.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/EventPoster.java
@@ -1,16 +1,16 @@
-//package com.example.qrcheckin;
-//
-//import android.media.Image;
-//
-//import java.io.File;
-//
-//public class EventPoster extends Image {
-//    private Event event;
-//    private Event event; // Association with the Event class
-//
-//    // Constructor for creating a new EventPoster
-//    public EventPoster(File imageFile, Attendee uploader, Event event) {
-//        super(imageFile, uploader); // Call to the superclass (Image) constructor
-//        this.event = event;
-//    }
-//}
+package com.example.qrcheckin;
+
+import android.media.Image;
+
+import java.io.File;
+
+public class EventPoster extends Image {
+    private Event event;
+    private Event event; // Association with the Event class
+
+    // Constructor for creating a new EventPoster
+    public EventPoster(File imageFile, Attendee uploader, Event event) {
+        super(imageFile, uploader); // Call to the superclass (Image) constructor
+        this.event = event;
+    }
+}
