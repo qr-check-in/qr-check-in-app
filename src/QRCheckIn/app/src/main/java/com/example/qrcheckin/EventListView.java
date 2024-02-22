4cb0c084dda90f9256518bf26ee7ae538a49be60
slappy163 <105318087+slappy163@users.noreply.github.com>
2024-02-21 20:15:14 -0700

    Working on mainappbar actions

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/EventListView.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/EventListView.java
@@ -2,18 +2,43 @@ package com.example.qrcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EventListView extends AppCompatActivity {
+    ImageButton qrButton;
+    ImageButton eventButton;
+    ImageButton addEventButton;
+    ImageButton profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list_view);
 
+        qrButton = findViewById(R.id.qrButton);
+        eventButton = findViewById(R.id.calenderButton);
+        addEventButton = findViewById(R.id.addCalenderButton);
+        profileButton = findViewById(R.id.profileButton);
+
+//        eventButton.setEnabled(false);
+        eventButton.setPressed(true);       // https://stackoverflow.com/questions/9318331/keep-android-button-selected-state, 2024, Prompt: how  to keep a button selected
+        eventButton.setSelected(true);
+
         Toolbar toolbar = findViewById(R.id.eventListToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Ongoing Events");
+
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent event = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(event);
+            }
+        });
     }
 }
--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/MainActivity.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.qrcheckin;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton qrButton;
+    ImageButton eventButton;
+    ImageButton addEventButton;
+    ImageButton profileButton;
 
     private FirebaseFirestore db;
 
@@ -17,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrButton = findViewById(R.id.qrButton);
+        qrButton.setPressed(true);
+        eventButton = findViewById(R.id.calenderButton);
+        addEventButton = findViewById(R.id.addCalenderButton);
+        profileButton = findViewById(R.id.profileButton);
 
         db = FirebaseFirestore.getInstance();
 
@@ -26,5 +40,13 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         TextView header = findViewById(R.id.mainHeader);
         header.setText("QRCheckIN");
+
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent event = new Intent(getApplicationContext(), EventListView.class);
+                startActivity(event);
+            }
+        });
     }
 }
