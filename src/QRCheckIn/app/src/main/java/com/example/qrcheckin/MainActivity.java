aa63cf96e521771f9c5a204a73db4ebbbf229f27
Sakib Rahman <73810792+glaucapicea@users.noreply.github.com>
2024-02-26 20:25:33 -0700

    Revert "Merge pull request #38 from CMPUT301W24T11/origin/Notifications"
    This reverts commit 30957d875409289f2413a29741193a6a69c69b4a, reversing
    changes made to 4e2278d1e48aa7fed483d03c7542c78dd9414dbb.

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/MainActivity.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.qrcheckin;
 
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,27 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_list_view);
 
         db = FirebaseFirestore.getInstance();
-        getToken();
-    }
 
-    private void getToken() {
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(Utils.TAG, "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
-
-                        // Get new FCM registration token
-                        String token = task.getResult();
-
-                        // Log and toast
-                        //String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d(Utils.TAG, token);
-                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 }
--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Utils.java
+++
@@ -1,5 +0,0 @@
-package com.example.qrcheckin;
-
-public class Utils {
-    public static final String TAG = "MyFirebaseMessagingService";
-}
