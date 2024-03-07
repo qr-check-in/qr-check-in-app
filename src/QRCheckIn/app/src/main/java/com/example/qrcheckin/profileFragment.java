32d2cbb24a084ea68e06c1982602393c22878aab
mcmclean4 <mcmclean@ualberta.ca>
2024-03-07 00:05:07 -0700

    profilefragment shows attributes
    (except profilepicture). Geolocation switch updates attendee trackGeolocation field in firestore

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/profileFragment.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/profileFragment.java
@@ -3,12 +3,23 @@ package com.example.qrcheckin;
 import static com.example.qrcheckin.R.layout.show_profile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class profileFragment extends AppCompatActivity {
     ImageButton qrButton;
@@ -17,6 +28,12 @@ public class profileFragment extends AppCompatActivity {
     ImageButton profileButton;
     Button updatePicture;
     Button removePicture;
+    TextView tvName;
+    TextView tvContact;
+    TextView tvHomepage;
+    Switch switchGeolocation;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference attendeesRef = db.collection("Attendees");
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +44,28 @@ public class profileFragment extends AppCompatActivity {
         profileButton = findViewById(R.id.profileButton);
         profileButton.setPressed(true);
         updatePicture = findViewById(R.id.btnUpdatePicture);
+
+        tvName = findViewById(R.id.name);
+        tvContact = findViewById(R.id.contact);
+        tvHomepage = findViewById(R.id.homepage);
+        switchGeolocation = findViewById(R.id.geoswitch);
+
+        // Get the fcmToken of the Attendee
+        SharedPreferences prefs = getSharedPreferences("TOKEN_PREF", MODE_PRIVATE);
+        String fcmToken = prefs.getString("token", "missing token");
+        Log.d("Firestore", String.format("TEST TOKEN STRING '%s'", fcmToken));
+        // set the profile attribute fields
+        setProfileFields(fcmToken);
+
+        switchGeolocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Database database = new Database();
+                database.updateAttendeeGeolocation(fcmToken, isChecked);
+            }
+        });
+
+
         eventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +90,30 @@ public class profileFragment extends AppCompatActivity {
         });
         updatePicture.setOnClickListener(v -> {
             new updatePictureFragment().show(getSupportFragmentManager(), "Update Picture");
+        });
+    }
+    public void setProfileFields(String fcmToken){
+        DocumentReference docRef = attendeesRef.document(fcmToken);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Attendee attendee = documentSnapshot.toObject(Attendee.class);
+                if(attendee == null){
+                    Log.e("Firestore", "Attendee doc not found");
+                }
+                else{
+                    // set fields for profile
+                    Log.d("Firestore", String.format("Attendee with name (%s) retrieved", attendee.getProfile().getName()));
+                    Profile profile = attendee.getProfile();
+                    String nameText = getResources().getString(R.string.profile_name_text, profile.getName());
+                    String contactText = getResources().getString(R.string.profile_contact_text, profile.getContact());
+                    String homepageText = getResources().getString(R.string.profile_homepage_text, profile.getHomepage());
+                    tvName.setText(nameText);
+                    tvContact.setText(contactText);
+                    tvHomepage.setText(homepageText);
+                    switchGeolocation.setChecked(profile.getTrackGeolocation());
+                }
+            }
         });
     }
 }
