08341540424bb6f0f84f823fdc17dcb038c28428
Sakib Rahman <73810792+glaucapicea@users.noreply.github.com>
2024-03-08 14:33:25 -0700

    Update QrCode

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/QRCode.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/QRCode.java
@@ -4,12 +4,19 @@ import android.net.Uri;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+
 public class QRCode extends Image {
     private String hashedContent;
 
-    public QRCode(Uri imageUri, Attendee uploader, String unhashedContent) {
-        super(imageUri, uploader);
-        this.hashedContent = hashString(unhashedContent);
+    /**
+     * Store a QR code with a generated ID linked to an event
+     * @param uriString String uri representation of an image
+     * @param uploader  Attendee object that uploaded the image
+     * @param unhashedContent Event details string in the form of [name][date][time][location]
+     */
+    public QRCode(Uri uriString, Attendee uploader, String unhashedContent) {
+        super(uriString, uploader);
+        this.hashedContent = Utils.hashString(unhashedContent);
     }
 
     public String getHashedContent() {
@@ -20,29 +27,4 @@ public class QRCode extends Image {
         this.hashedContent = hashedContent;
     }
 
-    /**
-     * Put event details through a hashing function to generate a unique ID for it and it's QR code
-     * @param unhashedContent Event details in the form of [name][date][time][location]
-     * @return String SHA-256 ID which uniquely represents an event
-     */
-    private String hashString(String unhashedContent) {
-        try {
-            // Create a MessageDigest instance for SHA-256
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
-            // Perform the hashing
-            byte[] encodedhash = digest.digest(unhashedContent.getBytes(java.nio.charset.StandardCharsets.UTF_8));
-
-            // Convert the hash into a hexadecimal string
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : encodedhash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
