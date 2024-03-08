eed010f801c555f7cf37cff65e3ffba91c3bcadb
Youssef Ibrahim <yibrahim@ualberta.ca>
2024-03-08 14:32:49 -0700

    Javadocs for  SharedViewModel, UpdatePictureFragment, Utils.

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Utils.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Utils.java
@@ -4,7 +4,10 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 import androidx.core.app.NotificationCompat;
-
+/**
+ * Utility class providing constants and utility methods related to FCM.
+ * Contains constants that are used throughout the app for consistency in FCM operations.
+ */
 public class Utils {
     // Tag used to identify the log message sent by MyFirebaseMessagingService
     public static final String TAG = "MyFirebaseMessagingService";
