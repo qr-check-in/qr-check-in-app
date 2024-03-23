8290a7ae0df3ca346a11c7d06032dcea3ddd5ff5
slappy163 <105318087+slappy163@users.noreply.github.com>
2024-03-22 18:22:52 -0600

    working on notification's page

---
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/CreateNotification.java
@@ -0,0 +1,19 @@
+package com.example.qrcheckin;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_create_notification);
+    }
+}
