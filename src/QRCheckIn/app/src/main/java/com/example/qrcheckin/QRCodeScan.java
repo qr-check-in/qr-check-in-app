7219e61e8720c63c220979aada222657843d75e4
Fumanpreet Singh <fumanpre@ualberta.ca>
2024-03-28 22:38:00 -0600

    fixed sudden profile closing

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/QRCodeScan.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/QRCodeScan.java
@@ -24,7 +24,10 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -33,8 +36,10 @@ import com.google.zxing.integration.android.IntentResult;
 import org.checkerframework.checker.units.qual.C;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 
 public class QRCodeScan extends AppCompatActivity {
@@ -53,7 +58,6 @@ public class QRCodeScan extends AppCompatActivity {
     private static final int REQUEST_CODE = 100;
     String longitude = null, latitude = null;
     private String attendeeFcm;
-    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -164,7 +168,7 @@ public class QRCodeScan extends AppCompatActivity {
 //                            Log.d("Document Id", "document id: " + documentId);
 
                             // Check the attendee into the event, update the event's attendees accordingly
-                            prefs = getSharedPreferences("TOKEN_PREF", MODE_PRIVATE);
+                            SharedPreferences prefs = getSharedPreferences("TOKEN_PREF", MODE_PRIVATE);
                             attendeeFcm = prefs.getString("token", "missing token");
                             AttendeeDatabaseManager attendeeDbManager = new AttendeeDatabaseManager(attendeeFcm);
                             EventDatabaseManager eventDbManager = new EventDatabaseManager(documentId);
@@ -181,12 +185,13 @@ public class QRCodeScan extends AppCompatActivity {
 
 
                             // Retrieve the boolean value of the "location" field under the "profile" field
-                            Boolean geoLocation = prefs.getBoolean("profile.trackGeolocation", false);
-                            Log.d("geoLocation", "geoLocation: " + geoLocation);
+//                            Boolean geoLocation = prefs.getBoolean("profile.trackGeolocation", false);
+//                            Log.d("geoLocation", "geoLocation: " + geoLocation);
 
 
                             // if organizer wants the user location
-                            if (checkInStatus && geoLocation) {
+//                            if (checkInStatus && geoLocation) {
+                            if (checkInStatus) {
                                 // call to get last location of user
                                 getLastLocation();
                             }
@@ -246,35 +251,44 @@ public class QRCodeScan extends AppCompatActivity {
                         Geocoder geocoder = new Geocoder(QRCodeScan.this, Locale.getDefault());
                         List<Address> addresses = null;
                         try {
-                            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            String coordinates = null;
-                            latitude = String.valueOf(addresses.get(0).getLatitude());
-                            longitude = String.valueOf(addresses.get(0).getLongitude());
+//                            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//                            String coordinates;
+//                            latitude = String.valueOf(addresses.get(0).getLatitude());
+//                            longitude = String.valueOf(addresses.get(0).getLongitude());
 
-                            coordinates = latitude + "," + longitude;
+//                            coordinates = latitude + "," + longitude;
+//
+//                            Log.d("COORDINATES", "coordinates: " + coordinates);
+//                            Log.d("attendeeFcm", "attendeeFcm: " + attendeeFcm);
 
-                            Log.d("COORDINATES", "coordinates: " + coordinates);
+                            // Convert latitude and longitude to GeoPoint
+                            GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
 
-//                            // Update the field location in the document
-//                            prefs.update("location", coordinates)
-//                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                        @Override
-//                                        public void onSuccess(Void aVoid) {
-//                                            // Field successfully updated
-//                                            // Handle success
-//                                        }
-//                                    })
-//                                    .addOnFailureListener(new OnFailureListener() {
-//                                        @Override
-//                                        public void onFailure(@NonNull Exception e) {
-//                                            // Throw a runtime exception to stop the program
-//                                            throw new RuntimeException("Firestore update failed: " + e.getMessage());
-//                                        }
-//                                    });
+                            // Get a reference to the Firestore document you want to update
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            DocumentReference docRef = db.collection("Attendees").document(attendeeFcm);
+
+                            // Update the document
+                            docRef.update("location", geoPoint)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            // Field successfully updated
+                                            // Handle success
+                                            Log.d("getLastLocation", "onSuccess: " + geoPoint);
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Handle failure
+                                            Log.e("Firestore", "Error updating document", e);
+                                        }
+                                    });
 
 
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
+                        } catch (Exception e) {
+                            Log.e("getLastLocation", "Error getting last location", e);
                         }
                     }
                 }
