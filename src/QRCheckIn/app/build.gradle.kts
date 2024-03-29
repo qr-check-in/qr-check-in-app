a336b2b2e9530f80ce66285cf0271cb44101f960
Fumanpreet Singh <fumanpre@ualberta.ca>
2024-03-28 18:01:59 -0600

    created maps activity

--- src/QRCheckIn/app/build.gradle.kts
+++ src/QRCheckIn/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -30,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -52,6 +56,7 @@ dependencies {
     implementation ("com.google.api:api-common:2.2.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("org.mockito:mockito-core:4.0.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.0.1")
