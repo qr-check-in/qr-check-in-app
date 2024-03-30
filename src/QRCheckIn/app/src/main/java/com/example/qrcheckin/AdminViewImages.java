18a987311286c1bfff0c66c5fecf153c7ccc582b
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-03-30 15:47:36 -0600

    Update AdminViewImages.java

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/AdminViewImages.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/AdminViewImages.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.qrcheckin;
 
 import android.content.Intent;
@@ -46,3 +47,18 @@ public class AdminViewImages extends AppCompatActivity {
         });
     }
 }
+=======
+//package com.example.qrcheckin;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class AdminViewImages extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.admin_browse_images);
+//    }
+//}
+>>>>>>> Stashed changes
