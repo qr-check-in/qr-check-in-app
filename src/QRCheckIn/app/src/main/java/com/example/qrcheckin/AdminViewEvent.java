0ed28c9d6340f86fc3b1e638e67bf35d2e5d97b2
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-03-30 15:47:32 -0600

    Update AdminViewEvent.java

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/AdminViewEvent.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/AdminViewEvent.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.qrcheckin;
 
 import android.content.Intent;
@@ -80,3 +81,19 @@ public class AdminViewEvent extends AppCompatActivity {
         eventAdapter.stopListening();
     }
 }
+=======
+//package com.example.qrcheckin;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class AdminViewEvent extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.browse_events_admin);
+//    }
+//}
+>>>>>>> Stashed changes
