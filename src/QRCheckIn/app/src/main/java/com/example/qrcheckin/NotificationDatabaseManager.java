bd2ed843b4ae14671c49cf1d65bae0d725b0c4d0
slappy163 <105318087+slappy163@users.noreply.github.com>
2024-04-01 20:45:21 -0600

    Update NotificationDatabaseManager.java

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/NotificationDatabaseManager.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/NotificationDatabaseManager.java
@@ -1,4 +1,24 @@
 package com.example.qrcheckin;
 
+import android.util.Log;
+
+/**
+ * Controls storing and retrieving data from the notifications firebase Collection
+ */
 public class NotificationDatabaseManager extends DatabaseManager{
+    /**
+     * Constructor NotificationDatabaseManager object for cases where we want to retrieve a Notification already in db
+     */
+    public NotificationDatabaseManager(String docID){
+        super("Notifications", docID);
+    }
+
+    /**
+     * Stores a Notification object to the Notification collection
+     * @param notification the notification to be stored
+     */
+    public void storeNotification(Notification notification){
+        getCollectionRef().document().set(notification);
+        Log.d("Firestore", String.format("Notification(%s) stored", notification.getTitle()));
+    }
 }
