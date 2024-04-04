dbdb94ad558a4fd8d0ae6f9f733df8b9c629c1a6
slappy163 <105318087+slappy163@users.noreply.github.com>
2024-04-04 16:19:34 -0600

    poo

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Notifications/MyNotificationManager.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Notifications/MyNotificationManager.java
@@ -59,7 +59,7 @@ public class MyNotificationManager {
         PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, channelId)
-                .setSmallIcon(R.drawable.app_icon_round)
+                .setSmallIcon(R.drawable.app_icon_resize_24)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setAutoCancel(true)
