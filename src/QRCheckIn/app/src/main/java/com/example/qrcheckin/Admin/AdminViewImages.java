6b47a41e6a5a1a8590bb2543de24392a604fa869
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-04-08 15:33:51 -0600

    added javadocs

--- src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Admin/AdminViewImages.java
+++ src/QRCheckIn/app/src/main/java/com/example/qrcheckin/Admin/AdminViewImages.java
@@ -23,7 +23,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * Activity for displaying a grid of images fetched from Firestore,
+ * including profile pictures and event posters.
+ */
 public class AdminViewImages extends AppCompatActivity implements ImageAdapter.OnImageClickListener {
     private RecyclerView imagesRecyclerView;
     ImageButton qrButton;
@@ -56,7 +59,10 @@ public class AdminViewImages extends AppCompatActivity implements ImageAdapter.O
         fetchAndDisplayImages();
 
     }
-
+    /**
+     * Fetches images from Firestore, including profile pictures of attendees
+     * and event posters, then displays them in the RecyclerView.
+     */
     private void fetchAndDisplayImages() {
         db.collection("Attendees").get()
                 .addOnSuccessListener(attendeesSnapshots -> {
@@ -89,6 +95,12 @@ public class AdminViewImages extends AppCompatActivity implements ImageAdapter.O
                 });
     }
 
+    /**
+     * Handles clicks on images displayed in the RecyclerView.
+     *
+     * @param image    The Image object representing the clicked image.
+     * @param position The position of the clicked image in the grid.
+     */
     @Override
     public void onImageClick(Image image, int position) {
         String folderName = imageUriToFolderMap.get(image);
